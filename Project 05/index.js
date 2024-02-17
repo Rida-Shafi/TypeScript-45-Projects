8db2b10a48afbd683606107208bcfd5c2ac28c58
Rida-Shafi <shafirida29+github@gmail.com>
2024-02-18 03:00:47 +0500

    complete project 05

---
+++ Project 05/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+// Muhammad Ali Jinnah once said, “Expect the best, prepare for the worst.”
+let famous_person = "Muhammad Ali Jinnah";
+let message = famous_person + ' once said, “Expect the best, prepare for the worst.”';
+console.log(message);
