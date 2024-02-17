5987bf8820c3405b4a5e999ba3efb7023019d429
Rida-Shafi <shafirida29+github@gmail.com>
2024-02-18 03:14:37 +0500

    complete project 21

---
+++ Project 21/index.js
@@ -0,0 +1,41 @@
+"use strict";
+/*They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+*/
+const bouquet = {
+    name: "Springn Delight",
+    price: 250,
+    description: "Beautiful arrangment of spring flowers"
+};
+//DEfine an arry of objects
+let bouquets = [];
+//pushing multiple object
+bouquets.push(bouquet);
+bouquets.push({
+    name: "summerbliss",
+    price: 350,
+    description: "beautiful arrangment of summer flowers"
+});
+let bouquet3 = {
+    name: "Red Hot",
+    price: 350,
+    description: "beautiful arrangment of red roses"
+};
+bouquets.push(bouquet3);
+// console.log(bouquets);
+bouquets.push({
+    name: "Freshness",
+    price: 250,
+    description: "A beautiful arrangment of white and yellow roses"
+});
+// console.log(bouquets);
+// Function to display bouquets
+function displayBouquets(bouquets) {
+    for (let i of bouquets) {
+        console.log(`title: ${i.name}
+        description ${i.description}
+        price: ${i.price}
+        ___________/n`);
+    }
+    ;
+}
+displayBouquets(bouquets);
