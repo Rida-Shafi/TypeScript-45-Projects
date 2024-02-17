1847f686db275199186385d5d114f976e9f8023b
Rida-Shafi <shafirida29+github@gmail.com>
2024-02-18 03:13:05 +0500

    complete project 19

---
+++ Project 19/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// tsc -w
+Object.defineProperty(exports, "__esModule", { value: true });
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+// Importing Guests List from Exercise 15
+const Project_15_1 = require("..//Project 15");
+console.log(`\nPrinting message indicating number of guests`);
+console.log(`We had finally invited ${Project_15_1.guestList.length} guest in Exercise 15`);
---
+++ Project 19/index.ts
@@ -0,0 +1,9 @@
+// tsc -w
+
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+// Importing Guests List from Exercise 15
+import{guestList} from "..//Project 15"
+
+console.log(`\nPrinting message indicating number of guests`)
+console.log(`We had finally invited ${guestList.length} guest from Exercise 15`)
